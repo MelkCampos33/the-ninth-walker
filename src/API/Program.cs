3dc4bf00546d52197788689fc6146d7ade998af0
Melk <melquisedequecampos.trabalho@gmail.com>
2026-03-28 01:59:28 -0300

    o sistema estava forçando a usar o SDK 10, mas foi feita a atualição no csprojs corretamente para o SDK 8 e criado um json global travando o sistema ao 8

--- src/API/Program.cs
+++ src/API/Program.cs
@@ -1,11 +1,29 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
+using Core.Models;
+using Core.Service;
 
-Console.ForegroundColor = ConsoleColor.Cyan;
-Console.WriteLine(@"
-    /| __________________________________________________
-   O|===|________________________________________________>
-    \|    THE NINTH WALKER - ALPHA VERSION
-");
+namespace API
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(@"
+                /| __________________________________________________
+8===========O|===|___________________________________________________  >
+                \|    THE NINTH WALKER - ALPHA VERSION
+            ");
 
-Console.ResetColor();
+            // Teste de criação
+            Heroi heroi = new Heroi("Melk");
+            
+            Console.WriteLine($"\nHerói {heroi.Nome} nível {heroi.Nivel} pronto para a jornada!");
+            Console.WriteLine("Pressione qualquer tecla para sair...");
+            Console.ReadKey();
+        }
+    }
+}
+
 
